8b6ae665df71302626997a61b93ce3cb515882f9
Michelle Chou <chou.m.rdh@gmail.com>
2020-07-17 14:47:02 -0700

    added listheader css and caret

--- src/components/jumbotron/Jumbotron.js
+++ src/components/jumbotron/Jumbotron.js
@@ -5,7 +5,7 @@ const Jumbotron = () => {
     <div className="jumbotron text-center bg-dark text-white mr-2 ml-2">
       <h1>Employee Directory</h1>
       <h5>
-        Click on carrots to filter by heading or use the search box to narrow
+        Click on caret to filter by heading or use the search box to narrow
         your results
       </h5>
     </div>
--- src/components/listHeader/ListHeader.js
+++ src/components/listHeader/ListHeader.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import "./ListHeader.css"
 
 
 function ListHeader(props){
     return(
         <div className ="card-header row text-center mr-2 ml-2">
             <p className ="col-sm-2 col-md-2 col-lg-2">Image</p>
-            <p className ="col-sm-2 col-md-2 col-lg-2" onClick = {()=>{props.onNameClick()}}>Name</p>
+            <p className ="col-sm-2 col-md-2 col-lg-2 " onClick = {()=>{props.onNameClick()}}>Name<span className="dropdown-toggle"></span></p>
             <p className ="col-sm-3 col-md-3 col-lg-3">Phone</p>
             <p className ="col-sm-3 col-md-3 col-lg-3">Email</p>
             <p className ="col-sm-2 col-md-2 col-lg-2">DOB</p>
