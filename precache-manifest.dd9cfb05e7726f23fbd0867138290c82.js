260ff469dda34796b0bc2fb8ed7b7b01b72b5ddc
Michelle Chou <chou.m.rdh@gmail.com>
2020-07-17 15:25:34 -0700

    Updates

---
+++ precache-manifest.dd9cfb05e7726f23fbd0867138290c82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6adeaccc4f3e417274fff92bd5d62f13",
+    "url": "/19_React_UserDirectory/index.html"
+  },
+  {
+    "revision": "adbfa92d5634a456386e",
+    "url": "/19_React_UserDirectory/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "75cfcb96b92f23694efe",
+    "url": "/19_React_UserDirectory/static/css/main.aa35c9ec.chunk.css"
+  },
+  {
+    "revision": "adbfa92d5634a456386e",
+    "url": "/19_React_UserDirectory/static/js/2.64cad8ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/19_React_UserDirectory/static/js/2.64cad8ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75cfcb96b92f23694efe",
+    "url": "/19_React_UserDirectory/static/js/main.fb0a17e2.chunk.js"
+  },
+  {
+    "revision": "e3ab57db5a946ac9f111",
+    "url": "/19_React_UserDirectory/static/js/runtime-main.06eaa508.js"
+  }
+]);
